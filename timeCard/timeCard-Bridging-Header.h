b96007fe9c094f9a06e35a47fd810fd8f5e46103
kazuhirosaji <kazuhirosaji@gmail.com>
2014-11-13 23:28:20 +0900

    prepare using sqlite3

---
+++ timeCard/timeCard-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  timeCard-Bridging-Header.h
+//  timeCard
+//
+//  Created by 佐治和弘 on 2014/11/13.
+//  Copyright (c) 2014年 kazuhirosaji. All rights reserved.
+//
+
+#import <sqlite3.h>
+
+#ifndef timeCard_timeCard_Bridging_Header_h
+#define timeCard_timeCard_Bridging_Header_h
+
+
+#endif
